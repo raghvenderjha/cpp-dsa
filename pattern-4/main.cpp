a6b2bfd489d24ec94f50f4b3075318f443df57e2
raghvenderjha <96420063+raghvenderjha@users.noreply.github.com>
2021-12-20 20:07:58 +0530

    raghvender-jha/pattern-4

---
+++ pattern-4/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+     int n;
+     cout<<"ENTER THE VALUE OF N:"<<endl;
+       cin>>n;
+     int star=n;
+     int space=0;
+         for(int i=1;i<=n;i++)
+         {
+             for(int j=1;j<=space;j++)
+             {
+                 cout<<"\t";
+
+             }
+             for(int j=1;j<=star;j++)
+             {
+                 cout<<"*"<<"\t";
+             }
+             cout<<endl;
+             space++;
+             star--;
+         }
+}
