549523480581f1657ad920788b91a203c66f2048
raghvenderjha <96420063+raghvenderjha@users.noreply.github.com>
2021-12-20 20:20:48 +0530

    raghvender-jha/pattern-19

---
+++ pattern-19/main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+     int n;
+     cout<<"ENTER THE VALUE OF N:"<<endl;
+        cin>>n;
+       for(int i=1;i<=n;i++)
+       {
+           for(int j=1;j<=n;j++)
+           {
+               if(i==1)
+               {
+                   if(j<=n/2+1||j==n)
+                   {
+                       cout<<"*"<<"\t";
+                   }
+                   else
+                   {
+                       cout<<"\t";
+                   }
+               }
+               else if(i<n/2+1)
+               {
+                   if(j==n/2+1||j==n)
+                   {
+                       cout<<"*"<<"\t";
+                   }
+                   else
+                   {
+                       cout<<"\t";
+                   }
+               }
+               else if(i==n/2+1)
+               {
+                   cout<<"*"<<"\t";
+               }
+               else if(i<n)
+               {
+                   if(j==1||j==n/2+1)
+                   {
+                       cout<<"*"<<"\t";
+                   }
+                   else
+                   {
+                       cout<<"\t";
+                   }
+               }
+               else
+               {
+                   if(j==1||j>=n/2+1)
+                   {
+                       cout<<"*"<<"\t";
+                   }
+                   else
+                   {
+                       cout<<"\t";
+                   }
+               }
+
+           }
+           cout<<endl;
+       }
+}
