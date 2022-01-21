dfa82cc47e376d77e00558ab33b8fb8c82b764ae
raghvenderjha <96420063+raghvenderjha@users.noreply.github.com>
2022-01-21 23:24:33 +0530

    Add files via upload

---
+++ LinkedList/DisplayALinkedList/main.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+      class node
+      {
+      public:
+        int data;
+        node*next;
+      };
+
+      class LinkedList
+      {
+      public:
+        node*head,*tail;
+        int size=0;
+
+        void addLast(int val)
+        {
+            node*temp=new node;
+            temp->data=val;
+            temp->next=NULL;
+            if(size==0)
+            {
+                head=temp;
+                tail=temp;
+            }
+            else
+            {
+                tail->next=temp;
+                tail=tail->next;
+            }
+            size++;
+        }
+        void display()
+        {
+            for(node*temp=head;temp!=NULL;temp=temp->next)
+            {
+                cout<<temp->data<<"\t";
+            }
+            cout<<endl;
+        }
+      };
+    int main()
+    {
+        LinkedList l1;
+        string s;
+        while(s!="quit")
+        {
+            if(s=="addLast")
+            {
+                int val;
+                cin>>val;
+                l1.addLast(val);
+            }
+            else if(s=="display")
+            {
+                l1.display();
+            }
+            else if(s=="size")
+            {
+                cout<<l1.size<<endl;
+            }
+            cin>>s;
+        }
+        return 0;
+    }
+
+
+
+
