ab44af3d2d13c25dad765ed0b4bb9abdf1beb3ad
RAGHVENDER JHA <96420063+raghvenderjha@users.noreply.github.com>
2022-07-17 09:03:32 +0530

    Add files via upload

---
+++ genericTree/genericTree1/jhg/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("NAME     : RAGHVENDER JHA\n");
+    printf("ROLL NO. : 129-IT-C\n");
+    int n,m;
+    printf("Enter the number of rows");
+    scanf("%d",&n);
+    m=n;
+   for(int i=1;i<=n;i++)
+   {
+       for(int j=1;j<i;j++)
+       {
+           printf(" ");
+       }
+       for(int k=1;k<=m;k++)
+       {
+           printf("*");
+       }
+       m--;
+
+      printf("\n");
+    }
+    return 0;
+}
