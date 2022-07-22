ce2656c53134ead715a2fa4cae41f673d3e96499
RAGHVENDER JHA <96420063+raghvenderjha@users.noreply.github.com>
2022-07-22 10:18:04 +0530

    Add files via upload

---
+++ LINKED LIST/GET VAL/main.cpp
@@ -0,0 +1,126 @@
+#include <bits/stdc++.h>
+
+ using namespace std;
+
+    class node
+    {
+    public:
+        int data;
+        node*next;
+    };
+
+    class linkedlist
+    {
+    public:
+        node*head,*tail;
+        int size=0;
+
+    public:
+        linkedlist()
+        {
+            head=NULL;
+            tail=NULL;
+        }
+        void addLast(int val)
+        {
+            node*temp=new node;
+            temp->data=val;
+            temp->next=NULL;
+            if(size==0)
+            {
+                head=temp;
+                tail=temp;
+            }
+            else
+            {
+                tail->next=temp;
+                tail=tail->next;
+            }
+            size++;
+        }
+        void getFirst()
+        {
+            if(size==0)
+            {
+                cout<<"LIST IS EMPTY"<<endl;
+            }
+            else{
+            cout<<head->data<<endl;
+            }
+        }
+        void getLast()
+        {
+            if(size==0)
+            {
+                cout<<"LIST IS EMPTY"<<endl;
+            }
+            else{
+            cout<<tail->data<<endl;
+            }
+        }
+        void getAt(int idx)
+        {
+            if(idx<0||idx>=size)
+            {
+                cout<<"INVALID ARGUMENTS"<<endl;
+            }
+            else if(idx==0)
+            {
+                getFirst();
+            }
+            else if(idx==size-1)
+            {
+                getLast();
+            }
+            else{
+            node*temp=head;
+            for(int i=0;i<idx;i++)
+            {
+                temp=temp->next;
+            }
+            cout<<temp->data<<endl;
+            }
+        }
+        void display()
+        {
+            for(node*temp=head;temp!=NULL;temp=temp->next)
+            {
+                cout<<temp->data<<"\t";
+            }
+            cout<<endl;
+        }
+    };
+  int main()
+  {
+      linkedlist l1;
+      string s;
+      while(s!="quit")
+      {
+          if(s=="addLast")
+          {
+              int val;
+              cin>>val;
+              l1.addLast(val);
+          }
+          else if(s=="getFirst")
+          {
+              l1.getFirst();
+          }
+          else if(s=="getLast")
+          {
+              l1.getLast();
+          }
+          else if(s=="getAt")
+          {
+              int idx;
+              cin>>idx;
+              l1.getAt(idx);
+          }
+          else if(s=="display")
+          {
+              l1.display();
+          }
+          cin>>s;
+      }
+      return 0;
+  }
