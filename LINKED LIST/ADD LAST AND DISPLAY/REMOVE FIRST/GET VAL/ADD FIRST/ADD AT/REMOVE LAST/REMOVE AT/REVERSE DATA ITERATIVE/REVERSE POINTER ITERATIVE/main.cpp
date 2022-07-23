e935f12b351411c3f5712cac5c458db734f8e091
RAGHVENDER JHA <96420063+raghvenderjha@users.noreply.github.com>
2022-07-23 09:35:30 +0530

    Add files via upload

---
+++ LINKED LIST/ADD LAST AND DISPLAY/REMOVE FIRST/GET VAL/ADD FIRST/ADD AT/REMOVE LAST/REMOVE AT/REVERSE DATA ITERATIVE/REVERSE POINTER ITERATIVE/main.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+
+ using namespace std;
+
+    class node
+    {
+    public:
+        int data;
+        node*next;
+    };
+
+    class linkedlist
+    {
+        public:
+        node*head,*tail;
+        int size=0;
+
+        public:
+            linkedlist()
+            {
+                head=NULL;
+                tail=NULL;
+            }
+            void addLast(int val)
+            {
+                node*temp=new node;
+                temp->data=val;
+                temp->next=NULL;
+                if(size==0)
+                {
+                    head=temp;
+                    tail=temp;
+                }
+                else
+                {
+                    tail->next=temp;
+                    tail=tail->next;
+                }
+                size++;
+            }
+            void reversePi()
+            {
+                node*prev=NULL;
+                node*curr=head;
+
+                while(curr!=NULL)
+                {
+                    node*next=curr->next;
+                    curr->next=prev;
+                    prev=curr;
+                    curr=next;
+                }
+
+                node*temp=head;
+                head=tail;
+                tail=temp;
+            }
+            void display()
+            {
+                for(node*temp=head;temp!=NULL;temp=temp->next)
+                {
+                    cout<<temp->data<<"\t";
+                }
+                cout<<endl;
+            }
+    };
+  int main()
+  {
+      linkedlist l1;
+      string s;
+      while(s!="quit")
+      {
+          if(s=="addLast")
+          {
+              int val;
+              cin>>val;
+              l1.addLast(val);
+          }
+          else if(s=="reversePi")
+          {
+              l1.reversePi();
+          }
+          else if(s=="display")
+          {
+              l1.display();
+          }
+          cin>>s;
+      }
+      return 0;
+  }
